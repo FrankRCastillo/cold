0766609c7fd87842de7e7d61ae8de851dabbf8e9
Franklin Castillo <franklin.r.castillo.3@gmail.com>
2024-04-11 13:32:33 -0400

    updated structure

--- setup.py
+++ setup.py
@@ -5,11 +5,13 @@ setup( name = 'cold'
      , description = 'CoLD: Command Line Downloader. Scrape websites and download files from the command line.'
      , packages = find_packages()
      , include_package_data = True
+     , package_data = { 'cold' : [ 'config/*']
+                      }
      , install_requires = [ 'lxml>=4.0.0'
                           , 'requests>=2.0.0'
                           ]
      , py_modules = ['cold']
-     , entry_points = { 'console_scripts' : ['cold=cold:main']
+     , entry_points = { 'console_scripts' : ['cold=cold.cold:main']
                       ,
                       }
      , url = "https://github.com/FrankRCastillo/cold"
